1a2b9675012b11e579c7ce94aee1d330e316cba4
Luciano Damian Mostaccio <lucianomostaccio@gmail.com>
2024-06-30 16:42:38 -0300

    changed to manually filling of categories in navbar

--- src/routers/api/products.router.js
+++ src/routers/api/products.router.js
@@ -6,18 +6,22 @@ import {
   deleteController,
   putController,
   addToCartController,
+  // getFilteredAndSortedProductsController,
+  // getCategoriesController,
 } from "../../controllers/products.controller.js";
 import { authenticateWithJwt } from "../../middlewares/authentication.js";
 
 export const productsRouter = Router();
 
-productsRouter.get("/", getController); 
-productsRouter.get("/:pid", getController); 
-productsRouter.post("/", postController); 
-productsRouter.put("/:pid", putController); 
-productsRouter.delete("/:pid", deleteController); 
+productsRouter.get("/", getController);
+productsRouter.get("/:pid", getController);
+// productsRouter.get("/filtered", getFilteredAndSortedProductsController);
+// productsRouter.get("/categories", getCategoriesController);
+productsRouter.post("/", postController);
+productsRouter.put("/:pid", putController);
+productsRouter.delete("/:pid", deleteController);
 productsRouter.post(
-  "/:pid/add-to-cart", 
+  "/:pid/add-to-cart",
   authenticateWithJwt, //protect the endpoint with the auth middleware
   addToCartController
 );
