327348462820129f8fd86f59077e6de18298b9d8
Luciano Damian Mostaccio <lucianomostaccio@gmail.com>
2024-07-07 16:53:05 -0300

    fix on product's cards on cart page

--- src/static/js/products.js
+++ src/static/js/products.js
@@ -3,25 +3,3 @@ document.addEventListener("DOMContentLoaded", function () {
   // @ts-ignore
   main.classList.remove("flex", "items-center", "justify-center");
 });
-
-// function addProductToCart(event, productId) {
-//   event.preventDefault();
-
-//   fetch(`/api/carts/`, {
-//     method: "PUT",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       action: "addProduct",
-//       productId: productId,
-//     }),
-//   })
-//     .then((response) => response.json())
-    // .then((data) => {
-    //   window.location.reload();
-    // })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//     });
-// }
