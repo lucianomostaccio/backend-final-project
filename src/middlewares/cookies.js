04d739b6651007e6c7fb85ea523c58d9d93576a2
Luciano Damian Mostaccio <lucianomostaccio@gmail.com>
2024-09-22 01:22:26 -0300

    csrf middleware implementation to avoid  cross-site request forgery (CSRF)

--- src/middlewares/cookies.js
+++ src/middlewares/cookies.js
@@ -1,4 +1,8 @@
 import cookieParser from "cookie-parser";
 import { COOKIE_SECRET } from "../config/config.js";
+import csrf from "lusca/lib/csrf";
 
 export const cookies = cookieParser(COOKIE_SECRET);
+export const csrfProtection = csrf({
+    cookie: true, 
+});
