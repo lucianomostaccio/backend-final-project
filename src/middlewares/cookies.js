123767499ee0dffee7cbb988fc8ddfc44f0cbb82
Luciano Damian Mostaccio <lucianomostaccio@gmail.com>
2024-09-22 01:28:03 -0300

    minor fix

--- src/middlewares/cookies.js
+++ src/middlewares/cookies.js
@@ -1,6 +1,4 @@
 import cookieParser from "cookie-parser";
 import { COOKIE_SECRET } from "../config/config.js";
-import { csrf } from "lusca";
 
 export const cookies = cookieParser(COOKIE_SECRET);
-export const csrfProtection = csrf();
